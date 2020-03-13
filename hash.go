618524ef9b7696dfdba1f28029077e18f1983a90
Freezingsmile <409871419@qq.com>
2020-03-13 15:59:38 +0800

    Add files via upload

---
+++ hash.go
@@ -0,0 +1,22 @@
+package gm
+
+import (
+	"hash"
+
+	"github.com/tjfoc/hyperledger-fabric-gm/bccsp"
+)
+
+//定义hasher 结构体，实现内部的一个 Hasher 接口
+type hasher struct {
+	hash func() hash.Hash
+}
+
+func (c *hasher) Hash(msg []byte, opts bccsp.HashOpts) (hash []byte, err error) {
+	h := c.hash()
+	h.Write(msg)
+	return h.Sum(nil), nil
+}
+
+func (c *hasher) GetHash(opts bccsp.HashOpts) (h hash.Hash, err error) {
+	return c.hash(), nil
+}
